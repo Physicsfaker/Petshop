09b983b7cd5777adce40bd85569b8e1645bcdf29
Online Physicsfacker <yourbraid@gmail.com>
2019-07-16 21:09:23 +0300

    Add project files.

---
+++ Petshop/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Petshop
+{
+    class Client
+    {
+        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
+        public Client(TcpClient Client)
+        {
+            // Код простой HTML-странички
+            //string Html = "<html><body><h1>It works!</h1></body></html>";
+            string Html = "Лови ответ сучка!";
+            // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+            string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            // Приведем строку к виду массива байт
+            byte[] Buffer = Encoding.UTF8.GetBytes(Str); 
+            // Отправим его клиенту
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            // Закроем соединение
+            Console.WriteLine("Получилось?");
+            Client.Close();
+        }
+    }
+}
---
+++ Petshop/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Petshop
+{
+
+    class PetshopServer
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+
+        public PetshopServer()
+        {
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, 80);
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    // Принимаем новых клиентов и передаем их на обработку новому экземпляру класса Client
+                    new Client(Listener.AcceptTcpClient());
+                }
+            });
+
+        }
+
+        // Остановка сервера
+
+        ~PetshopServer()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();     // Получение имени компьютера.
+            string host = Dns.GetHostEntry(hostName).AddressList[1].ToString();    // Получение ip-адреса.
+            //foreach (IPAddress address in host.AddressList)
+            //{
+            //    Console.WriteLine($"    {address}");
+            //}
+            Console.WriteLine(hostName);
+            Console.WriteLine(host);
+
+            new PetshopServer();
+
+            Task.Run(() =>
+            {
+                HttpWebRequest requestt = (HttpWebRequest)WebRequest.Create($@"http://{host}:80/g=123"); ///get 1
+                requestt.Credentials = CredentialCache.DefaultCredentials;
+                requestt.Method = "GET";
+                requestt.ContentType = "application/x-www-form-urlencoded";
+                WebResponse responsee = requestt.GetResponse();
+                Console.WriteLine("Отправилось...");
+                using (Stream stream = responsee.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+                responsee.Close();
+                Console.WriteLine("Все конченно!");
+            });
+
+            //WebRequest request = WebRequest.Create("http://localhost:5374/Home/PostData?sName=Иван Иванов&age=31"); //get2
+            //WebResponse response = await request.GetResponseAsync();
+            //using (Stream stream = response.GetResponseStream())
+            //{
+            //    using (StreamReader reader = new StreamReader(stream))
+            //    {
+            //        Console.WriteLine(reader.ReadToEnd());
+            //    }
+            //}
+            //response.Close();
+
+            //WebRequest request = WebRequest.Create("http://localhost:5374/Home/PostData");  //post
+            //request.Method = "POST";
+            //string sName = "sName=Иван Иванов&age=31";
+            //byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(sName);
+            //request.ContentType = "application/x-www-form-urlencoded";
+            //request.ContentLength = byteArray.Length;
+            //using (Stream dataStream = request.GetRequestStream())
+            //{
+            //    dataStream.Write(byteArray, 0, byteArray.Length);
+            //}
+
+
+            Console.ReadKey();
+        }
+    }
+}
