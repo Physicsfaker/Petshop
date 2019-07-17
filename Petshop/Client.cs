91d3a53159263dd8b297d405f17bfc341487c9be
Online Physicsfacker <yourbraid@gmail.com>
2019-07-17 21:57:30 +0300

    so close...

--- Petshop/Client.cs
+++ Petshop/Client.cs
@@ -2,33 +2,46 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Petshop
 {
     class Client
     {
-        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
         public Client(TcpClient Client)
         {
-            // Код простой HTML-странички
+            Console.WriteLine("Обработка сервером клиента");
+            // Объявим строку, в которой будет хранится запрос клиента
+            string Request = "";
+            // Буфер для хранения принятых от клиента данных
+            byte[] Buffer = new byte[1024];
+            // Переменная для хранения количества байт, принятых от клиента
+            int Count;
+            // Читаем из потока клиента до тех пор, пока от него поступают данные
+            while ((Count = Client.GetStream().Read(Buffer, 0, Buffer.Length)) > 0)
+            {
+                // Преобразуем эти данные в строку и добавим ее к переменной Request
+                Request += Encoding.ASCII.GetString(Buffer, 0, Count);
+                // Запрос должен обрываться последовательностью \r\n\r\n
+                // Либо обрываем прием данных сами, если длина строки Request превышает 4 килобайта
+                // Нам не нужно получать данные из POST-запроса (и т. п.), а обычный запрос
+                // по идее не должен быть больше 4 килобайт
+                Console.Write(Request);
+                if (Request.IndexOf("\r\n\r\n") >= 0 || Request.Length > 4096) break;
+            }
+            Console.WriteLine(Request);
+            //Match ReqMatch = Regex.Match(Request, @"^\w+\s+([^\s\?]+)[^\s]*\s+HTTP/.*|");
+
+            // Переменная для хранения количества байт, принятых от клиента
             //string Html = "<html><body><h1>It works!</h1></body></html>";
-            string Html = "Ловиответсучка!";
+            string Html = "It works!";
             // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
             string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
-            // Приведем строку к виду массива байт
-            byte[] Buffer = Encoding.UTF8.GetBytes(Str); 
-            // Отправим его клиенту
-            Client.GetStream().WriteAsync(Buffer, 0, Buffer.Length);
-            //GetStream(Buffer);
-            // Закроем соединение
-            Console.WriteLine("Получилось?");
-            //Client.Close();
+            Buffer = Encoding.ASCII.GetBytes(Str);
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            //Console.WriteLine("Hello World!");
+            Client.Close();
         }
-
-        //private void GetStream(byte[] buf) 
-        //{
-        //    Client.GetStream().Write(buf, 0, buf.Length);
-        //}
     }
 }
--- Petshop/Program.cs
+++ Petshop/Program.cs
@@ -4,78 +4,114 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
+/*Задание заключается в разработке HTTP-сервера «Petshop» в виде консольного приложения.
+ * Требования к результату следующие:
+ *   Чистое консольное приложение без использования asp.net api/mvc и прочего.
+ *   У питомца должны быть следующие поля:
+ *   "name": "string",
+ *   "type": "string".
+ *   На запрос POSТ /pets создавать нового питомца. 
+ *   На запрос GET /pets возвращать список всех питомцев.
+ *   На любой другой GET запрос он должен возвращать «Hello world!».
+ *   Можно не хранить данные в БД а использовать просто статичные переменные.
+ *   Будет плюсом если приложение сможет обрабатывать несколько запросов одновременно.*/
+
 namespace Petshop
 {
 
     class PetshopServer
     {
+        #region Server
         TcpListener Listener; // Объект, принимающий TCP-клиентов
-
-        // Запуск сервера
+        public static string host; //адрес сервера
 
         public PetshopServer()
         {
-            // Создаем "слушателя" для указанного порта
-            Listener = new TcpListener(IPAddress.Any, 80);
-            Listener.Start(); // Запускаем его
+            Listener = new TcpListener(IPAddress.Any, 80); // Создаем "слушателя" для указанного порта
+            Listener.Start();
 
-            // В бесконечном цикле
-
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    // Принимаем новых клиентов и передаем их на обработку новому экземпляру класса Client
-                    new Client(Listener.AcceptTcpClient());
-                }
-            });
-
+            Task.Run(() => { while (true) new Client(Listener.AcceptTcpClient()); });
         }
 
-        // Остановка сервера
+        ~PetshopServer() { if (Listener != null) Listener.Stop(); } // Остановка сервера
+        #endregion
 
-        ~PetshopServer()
+        public static void SendRequest(string command)
         {
-            // Если "слушатель" был создан
-            if (Listener != null)
-            {
-                // Остановим его
-                Listener.Stop();
-            }
-        }
+            command = command.ToLower();
+            WebRequest request;
 
+            if (command.ToLower().StartsWith("post /pets"))  //post
+            {
+                request = WebRequest.Create($@"http://{host}:80/");  
+                request.Method = "POST";
+                string param = "/pets";
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(param);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+                Console.WriteLine("Отправилось post...");
+            }
+            else if (command.ToLower().StartsWith("get ")) //get
+            {
+                request = WebRequest.Create($@"http://{host}:80?param=/pets"+ command);
+                request.Credentials = CredentialCache.DefaultCredentials;
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                Console.WriteLine("Отправилось get...");
+            }
+            else { Console.Clear(); Console.WriteLine("Invalid command!");  return; } //неверные команды
+
+            WebResponse responsee = request.GetResponse();
+            using (Stream stream = responsee.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            responsee.Close();
+            Console.WriteLine("Все конченно!");
+        }
 
         static void Main(string[] args)
         {
+            new PetshopServer(); //Создаем прослушку и сам сервер
+
             string hostName = Dns.GetHostName();     // Получение имени компьютера.
-            string host = Dns.GetHostEntry(hostName).AddressList[1].ToString();    // Получение ip-адреса.
-            //foreach (IPAddress address in host.AddressList)
-            //{
-            //    Console.WriteLine($"    {address}");
-            //}
-            Console.WriteLine(hostName);
-            Console.WriteLine(host);
-
-            new PetshopServer();
-
-            Task.Run(async () => 
+            foreach (IPAddress address in Dns.GetHostEntry(hostName).AddressList)
             {
-                HttpWebRequest requestt = (HttpWebRequest)WebRequest.Create($@"http://{host}:80/g=123"); ///get 1
-                requestt.Credentials = CredentialCache.DefaultCredentials;
-                requestt.Method = "GET";
-                requestt.ContentType = "application/x-www-form-urlencoded";
-                WebResponse responsee = await requestt.GetResponseAsync();
-                Console.WriteLine("Отправилось...");
-                using (Stream stream = responsee.GetResponseStream())
+                //Console.WriteLine($"    {address}");
+                if (address.ToString().Length >= 7) { host = address.ToString(); break; } //защитка от "::1" адресса
+            }
+            Console.WriteLine("host: " + host);
+
+            while (true)
+            {
+                //SendRequest(Console.ReadLine());
+                SendRequest("GET /pets");
+                Console.ReadKey();
+                SendRequest("POST /pets");
+                Console.ReadKey();
+            }
+
+            //Task.Run(async () =>
+            //{
+            WebRequest requestt = WebRequest.Create($@"http://{host}:80/g=123"); ///get 1
+            requestt.Credentials = CredentialCache.DefaultCredentials;
+            requestt.Method = "GET";
+            requestt.ContentType = "application/x-www-form-urlencoded";
+            WebResponse responsee = requestt.GetResponse();
+            Console.WriteLine("Отправилось...");
+            using (Stream stream = responsee.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
                 {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        Console.WriteLine(reader.ReadToEnd());
-                    }
+                    Console.WriteLine(reader.ReadToEnd());
                 }
-                responsee.Close();
-                Console.WriteLine("Все конченно!");
-            });
+            }
+            responsee.Close();
+            Console.WriteLine("Все конченно!");
+            //});
 
             //WebRequest request = WebRequest.Create("http://localhost:5374/Home/PostData?sName=Иван Иванов&age=31"); //get2
             //WebResponse response = await request.GetResponseAsync();
@@ -98,9 +134,6 @@ namespace Petshop
             //{
             //    dataStream.Write(byteArray, 0, byteArray.Length);
             //}
-
-
-            Console.ReadKey();
         }
     }
 }
